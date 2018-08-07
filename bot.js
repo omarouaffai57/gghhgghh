ef489ac4446e76f2386cddfad02b2e303b4bc778
omarouaffai57 <42187834+omarouaffai57@users.noreply.github.com>
2018-08-07 22:17:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,6 +53,19 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('^bcall')){
+ if (message.author.id !== '300972049847943169') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '300972049847943169') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
 
 
 
